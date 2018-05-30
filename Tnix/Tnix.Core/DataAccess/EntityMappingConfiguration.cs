b1b065356c201b6ffccdddba78c2953e2a1c62cd
thrinadha.rao <thrinadha.ithigani@tekzenit.com>
2018-05-30 17:57:43 +0530

    changes

---
+++ Tnix/Tnix.Core/DataAccess/EntityMappingConfiguration.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Tnix.Core.DataAccess
+{
+    //using System;
+    //using System.Collections.Generic;
+    //using System.Reflection;
+    //using Microsoft.EntityFrameworkCore;
+    //using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    //using System.Linq;
+
+
+    //public interface IEntityMappingConfiguration
+    //{
+    //    void Map(ModelBuilder b);
+    //}
+
+    //public interface IEntityMappingConfiguration<T> : IEntityMappingConfiguration where T : class
+    //{
+    //    void Map(EntityTypeBuilder<T> builder);
+    //}
+
+    class EntityMappingConfiguration
+    {
+    }
+}
